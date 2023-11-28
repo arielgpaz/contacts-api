9529ecdf28736007619962c117e97d74b0ea30ac
Ariel Gustavo <arielgdapaz@gmail.com>
2023-11-28 13:41:06 -0300

    Documentação com Swagger

--- src/main/java/com/teste/contactsapi/controllers/ContatosController.java
+++ src/main/java/com/teste/contactsapi/controllers/ContatosController.java
@@ -3,6 +3,8 @@ package com.teste.contactsapi.controllers;
 import com.teste.contactsapi.domain.ContatoRequest;
 import com.teste.contactsapi.domain.ContatoResponse;
 import com.teste.contactsapi.services.ContatosService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,13 @@ public class ContatosController {
     private final ContatosService contatosService;
 
     @GetMapping
+    @Operation(
+            summary = "Buscar contatos",
+            description = "Endpoint para buscas personalizadas")
     public List<ContatoResponse> buscarContatos(
+            @Parameter(description = "String que pode estar contida em qualquer campo dos contatos")
             @RequestParam String q,
+            @Parameter(description = "Campos que serão retornados")
             @RequestParam(required = false) List<String> fields
     ) {
 
@@ -26,28 +33,52 @@ public class ContatosController {
     }
 
     @GetMapping("/{id}")
-    public ContatoResponse buscarContato(@PathVariable Long id) {
+    @Operation(
+            summary = "Buscar contatos por id",
+            description = "Endpoint para buscar por id")
+    public ContatoResponse buscarContato(
+            @Parameter(description = "Identificador único do contato")
+            @PathVariable Long id
+    ) {
 
         return contatosService.buscarContatoResponsePorId(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ContatoResponse cadastrarContato(@RequestBody ContatoRequest contatoRequest) {
+    @Operation(
+            summary = "Cadastrar contatos",
+            description = "Endpoint para cadastrar novos contatos")
+    public ContatoResponse cadastrarContato(
+            @Parameter(description = "Informações do contato a ser cadastrado")
+            @RequestBody ContatoRequest contatoRequest
+    ) {
 
         return contatosService.cadastrarContato(contatoRequest);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Atualizar contatos",
+            description = "Endpoint para atualizar contatos já cadastrados")
     public void atualizarContato(
+            @Parameter(description = "Identificador único do contato")
             @PathVariable Long id,
+            @Parameter(description = "Novas informações do contato a ser atualizado")
             @RequestBody ContatoRequest contatoRequest
     ) {
+
         contatosService.atualizarContato(id, contatoRequest);
     }
 
     @DeleteMapping("/{id}")
-    public void excluirContato(@PathVariable Long id) {
+    @Operation(
+            summary = "Excluir contatos",
+            description = "Endpoint para excluir contatos que não serão mais usados")
+    public void excluirContato(
+            @Parameter(description = "Identificador único do contato")
+            @PathVariable Long id
+    ) {
 
         contatosService.excluirContato(id);
 
--- src/main/java/com/teste/contactsapi/controllers/ProfissionaisController.java
+++ src/main/java/com/teste/contactsapi/controllers/ProfissionaisController.java
@@ -3,6 +3,8 @@ package com.teste.contactsapi.controllers;
 import com.teste.contactsapi.domain.ProfissionalRequest;
 import com.teste.contactsapi.domain.ProfissionalResponse;
 import com.teste.contactsapi.services.ProfissionaisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,36 +19,66 @@ public class ProfissionaisController {
     private final ProfissionaisService profissionaisService;
 
     @GetMapping
+    @Operation(
+            summary = "Buscar profissionais",
+            description = "Endpoint para buscas personalizadas")
     public List<ProfissionalResponse> buscarProfissionais(
+            @Parameter(description = "String que pode estar contida em qualquer campo dos profissionais")
             @RequestParam String q,
+            @Parameter(description = "Campos que serão retornados")
             @RequestParam(required = false) List<String> fields
     ) {
+
         return profissionaisService.buscarProfissionaisPorFiltros(q, fields);
     }
 
     @GetMapping("/{id}")
-    public ProfissionalResponse buscarProfissional(@PathVariable Long id) {
+    @Operation(
+            summary = "Buscar profissionais por id",
+            description = "Endpoint para buscar por id")
+    public ProfissionalResponse buscarProfissional(
+            @Parameter(description = "Identificador único do profissional")
+            @PathVariable Long id
+    ) {
 
         return profissionaisService.buscarProfissionalResponsePorId(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProfissionalResponse cadastrarProfissional(@RequestBody ProfissionalRequest profissionalRequest) {
+    @Operation(
+            summary = "Cadastrar profissionais",
+            description = "Endpoint para cadastrar novos profissionais")
+    public ProfissionalResponse cadastrarProfissional(
+            @Parameter(description = "Informações do profissional a ser cadastrado")
+            @RequestBody ProfissionalRequest profissionalRequest
+    ) {
 
         return profissionaisService.cadastrarProfissional(profissionalRequest);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Atualizar profissionais",
+            description = "Endpoint para atualizar profissionais já cadastrados")
     public void atualizarProfissional(
+            @Parameter(description = "Identificador único do profissional")
             @PathVariable Long id,
+            @Parameter(description = "Novas informações do profissional a ser atualizado")
             @RequestBody ProfissionalRequest profissionalRequest
     ) {
+
         profissionaisService.atualizarProfissional(id, profissionalRequest);
     }
 
     @DeleteMapping("/{id}")
-    public void excluirProfissional(@PathVariable Long id) {
+    @Operation(
+            summary = "Excluir profissionais",
+            description = "Endpoint para exclusão lógica de profissionais que não serão mais usados")
+    public void excluirProfissional(
+            @Parameter(description = "Identificador único do profissional")
+            @PathVariable Long id
+    ) {
 
         profissionaisService.excluirProfissional(id);
     }
