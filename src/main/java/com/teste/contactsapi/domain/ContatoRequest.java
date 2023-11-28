d3d18ec56e956dcd6646300c2dbb0e2552b7298d
Ariel Gustavo <arielgdapaz@gmail.com>
2023-11-27 23:57:24 -0300

    Usar records

--- src/main/java/com/teste/contactsapi/domain/ContatoRequest.java
+++ src/main/java/com/teste/contactsapi/domain/ContatoRequest.java
@@ -1,24 +1,3 @@
 package com.teste.contactsapi.domain;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ContatoRequest {
-
-    @NotEmpty
-    private String nome;
-
-    @NotEmpty
-    private String contato;
-
-    @NotNull
-    private Long profissionalId;
-}
+public record ContatoRequest(String nome, String contato, Long profissionalId) { }
--- src/main/java/com/teste/contactsapi/domain/ProfissionalRequest.java
+++ src/main/java/com/teste/contactsapi/domain/ProfissionalRequest.java
@@ -1,23 +1,5 @@
 package com.teste.contactsapi.domain;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
-@Builder
-public class ProfissionalRequest {
-
-    @NotEmpty
-    private String nome;
-
-    @NotEmpty
-    private Cargo cargo;
-
-    @NotNull
-    private Date nascimento;
-
-}
+public record ProfissionalRequest(String nome, Cargo cargo, Date nascimento) {}
